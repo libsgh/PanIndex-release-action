b62b43fa58651085590522b18a5e12c9d9f37693
libsgh <woiyyng@gmail.com>
2021-01-12 15:56:21 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- setup-go.sh
+++ setup-go.sh
@@ -7,6 +7,8 @@ mv go /usr/local/
 mkdir -p /go/bin /go/src /go/pkg
 
 export GO_HOME=/usr/local/go
-export PATH=${GO_HOME}/bin/:$PATH
 export GOPATH=/go
+export GO111MODULE="on"
+export GOPROXY="https://goproxy.io"
+export PATH=${GO_HOME}/bin/:$PATH
 
