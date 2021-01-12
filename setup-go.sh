737760f0180aff43077ba81a0a317a44143c1da7
libsgh <woiyyng@gmail.com>
2021-01-12 17:52:44 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- setup-go.sh
+++ setup-go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 export GO111MODULE="on"
-export GOPROXY="https://goproxy.io"
-export PATH=/go/bin/:$PATH
+#export GOPROXY="https://goproxy.io"
+#export PATH=/go/bin/:$PATH
 go env
 
