9a43afa9bfbb11f672e41c1e196a415af40a4068
libsgh <woiyyng@gmail.com>
2021-01-12 19:16:32 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- release.sh
+++ release.sh
@@ -5,5 +5,30 @@ RELEASE_ASSETS_UPLOAD_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .release.upload_url
 RELEASE_ASSETS_UPLOAD_URL=${RELEASE_ASSETS_UPLOAD_URL%\{?name,label\}}
 git clone https://github.com/libsgh/PanIndex.git
 cd PanIndex
-xgo --targets=windows/*,darwin/*,linux/arm-7,linux/arm64,linux/386,linux/amd64
-ls -al
+#xgo --targets=windows/*,darwin/*,linux/arm-7,linux/arm64,linux/386,linux/amd64 github.com/libsgh/PanIndex
+#ls -al
+
+echo ${GITHUB_REF}
+RELEASE_TAG=$(basename ${GITHUB_REF})
+BUILD_ARTIFACTS_FOLDER=build-artifacts-$(date +%s)
+mkdir -p ${BUILD_ARTIFACTS_FOLDER}
+
+echo $RELEASE_TAG
+# linux-amd64
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex
+
+cp -r LICENSE README.md CHANGELOG.md config/config.json ${BUILD_ARTIFACTS_FOLDER}/
+cd ${BUILD_ARTIFACTS_FOLDER}
+ls -lha
+
+tar cvfz PanIndex-${RELEASE_TAG}-linux-amd64.tar.gz
+
+# update binary and checksum
+curl \
+  --fail \
+  -X POST \
+  --data-binary @PanIndex-${RELEASE_TAG}-linux-amd64.tar.gz \
+  -H 'Content-Type: application/gzip' \
+  -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
+  "${RELEASE_ASSETS_UPLOAD_URL}?name=PanIndex-${RELEASE_TAG}-linux-amd64.tar.gz"
+echo $?
--- setup-go.sh
+++ setup-go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
 export GO111MODULE="on"
+export GOPROXY="https://goproxy.io"
 export PATH=/go/bin/:$PATH
 go get github.com/karalabe/xgo
 go get -u github.com/gobuffalo/packr/v2/packr2
