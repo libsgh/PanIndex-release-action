0c4283a18e974796a57d7ee8d60548e6c8d94b2a
libsgh <woiyyng@gmail.com>
2021-01-12 18:32:24 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # prepare upload URL
 RELEASE_ASSETS_UPLOAD_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .release.upload_url)
 RELEASE_ASSETS_UPLOAD_URL=${RELEASE_ASSETS_UPLOAD_URL%\{?name,label\}}
-git clone https://github.com/libsgh/PanIndex.git
-cd PanIndex
+#git clone https://github.com/libsgh/PanIndex.git
+#cd PanIndex
 xgo --targets=windows/*,darwin/*,linux/arm-7,linux/arm64,linux/386,linux/amd64 github.com/libsgh/PanIndex
 ls -al
