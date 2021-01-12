d4a1b47630bb4c5d05e6cc34697d4cb9c04019a6
libsgh <woiyyng@gmail.com>
2021-01-12 15:07:36 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- release.sh
+++ release.sh
@@ -6,7 +6,9 @@ cd PanIndex
 
 go get -u github.com/gobuffalo/packr/v2/packr2
 
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 packr2 build -x -o dist/PanIndex
+packr2
+
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -x -o dist/PanIndex
 
 cd dist
 
