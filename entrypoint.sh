3374ab32ff1d688afbe519fd866ed7af84165e48
libsgh <woiyyng@gmail.com>
2021-01-13 10:40:07 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,13 @@ source /setup-go.sh
 go version
 
 # build & release go binaries
-/release.sh
+/release.sh linux 386
+/release.sh linux amd64
+/release.sh linux arm32-v7
+/release.sh linux arm64
+/release.sh windows 386
+/release.sh windows amd64
+/release.sh darwin 386
+/release.sh darwin amd64
+
 
--- release.sh
+++ release.sh
@@ -1,28 +1,75 @@
 #!/bin/bash -eux
 
 # prepare upload URL
-cat ${GITHUB_EVENT_PATH}
 RELEASE_ASSETS_UPLOAD_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .release.upload_url)
 RELEASE_ASSETS_UPLOAD_URL=${RELEASE_ASSETS_UPLOAD_URL%\{?name,label\}}
-git clone https://github.com/libsgh/PanIndex.git
-cd PanIndex
-#xgo --targets=windows/*,darwin/*,linux/arm-7,linux/arm64,linux/386,linux/amd64 github.com/libsgh/PanIndex
-#ls -al
 
-echo ${GITHUB_REF}
+cd ${INPUT_PROJECT_PATH}
+
 RELEASE_TAG=$(basename ${GITHUB_REF})
 BUILD_ARTIFACTS_FOLDER=build-artifacts-$(date +%s)
 mkdir -p ${BUILD_ARTIFACTS_FOLDER}
+# binary suffix
+EXT=''
+if [ $1 == 'windows' ]; then
+  EXT='.exe'
+  ASSETS_EXT='.zip'
+fi
+FILE_SUFFIX=$1-$2${ASSETS_EXT}
+if [ $1 == 'linux' ]&&[ $2 == '386' ]; then
+# linux-386
+CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
 
-echo $RELEASE_TAG
+if [ $1 == 'linux' ]&&[ $2 == 'amd64' ]; then
 # linux-amd64
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
 
-cp -r LICENSE README.md CHANGELOG.md config/config.json ${BUILD_ARTIFACTS_FOLDER}/
+if [ $1 == 'linux' ]&&[ $2 == 'arm32-v7' ]; then
+# linux-armv7-a
+CC=arm-linux-gnueabihf-gcc-5 CXX=arm-linux-gnueabihf-g++-5 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a -fPIC" CGO_CXXFLAGS="-march=armv7-a -fPIC" go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
+
+if [ $1 == 'linux' ]&&[ $2 == 'arm64' ]; then
+# linux-arm64
+CC=aarch64-linux-gnu-gcc-5 CXX=aarch64-linux-gnu-g++-5 GOOS=linux GOARCH=arm64 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
+
+if [ $1 == 'windows' ]&&[ $2 == 'amd64' ]; then
+# windows-amd64
+CC=x86_64-w64-mingw32-gcc-posix CXX=x86_64-w64-mingw32-g++-posix GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
+
+if [ $1 == 'windows' ]&&[ $2 == '386' ]; then
+# windows-386
+CC=i686-w64-mingw32-gcc-posix CXX=i686-w64-mingw32-g++-posix GOOS=windows GOARCH=386 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
+
+if [ $1 == 'darwin' ]&&[ $2 == 'amd64' ]; then
+# darwin-amd64
+CC=o64-clang CXX=o64-clang++ GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
+
+if [ $1 == 'darwin' ]&&[ $2 == '386' ]; then
+# darwin-386
+CC=o32-clang CXX=o32-clang++ GOOS=darwin GOARCH=386 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}}
+fi
+
+cp -r LICENSE README.md config/config.json ${BUILD_ARTIFACTS_FOLDER}/
 cd ${BUILD_ARTIFACTS_FOLDER}
-ls -lha
 
-tar cvfz PanIndex-${RELEASE_TAG}-linux-amd64.tar.gz  *
+if [ $1 == 'windows' ]; then
+zip -vr PanIndex-${RELEASE_TAG}-${FILE_SUFFIX} *
+else
+tar cvfz PanIndex-${RELEASE_TAG}-${FILE_SUFFIX} *
+fi
+
+MD5_SUM=$(md5sum PanIndex-${RELEASE_TAG}-${FILE_SUFFIX} | cut -d ' ' -f 1)
+SHA1_SUM=$(sha1sum PanIndex-${RELEASE_TAG}-${FILE_SUFFIX} | cut -d ' ' -f 1)
+SHA256_SUM=$(sha256sum PanIndex-${RELEASE_TAG}-${FILE_SUFFIX} | cut -d ' ' -f 1)
+SHA512_SUM=$(sha512sum PanIndex-${RELEASE_TAG}-${FILE_SUFFIX} | cut -d ' ' -f 1)
+
 
 # update binary and checksum
 curl \
@@ -31,5 +78,14 @@ curl \
   --data-binary @PanIndex-${RELEASE_TAG}-linux-amd64.tar.gz \
   -H 'Content-Type: application/gzip' \
   -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
-  "${RELEASE_ASSETS_UPLOAD_URL}?name=PanIndex-${RELEASE_TAG}-linux-amd64.tar.gz"
+  "${RELEASE_ASSETS_UPLOAD_URL}?name=PanIndex-${RELEASE_TAG}-${FILE_SUFFIX}"
+echo $?
+
+curl \
+  --fail \
+  -X POST \
+  --data MD5=${MD5_SUM}\nSHA1=${SHA1_SUM}\nSHA256=${SHA256_SUM}\nSHA512=${SHA512_SUM} \
+  -H 'Content-Type: text/plain' \
+  -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
+  "${RELEASE_ASSETS_UPLOAD_URL}?name=PanIndex-${RELEASE_TAG}-${FILE_SUFFIX}.dgst"
 echo $?
