86490c6bedf9e113df0132ade5081d0163e681b1
libsgh <woiyyng@gmail.com>
2021-02-05 18:12:35 +0800

    构建时增加版本信息输出
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ cd ${GITHUB_WORKSPACE}
 RELEASE_TAG=$(basename ${GITHUB_REF})
 BUILD_ARTIFACTS_FOLDER=build-artifacts-$(date +%s)
 mkdir -p ${BUILD_ARTIFACTS_FOLDER}
+flags="-X 'main.VERSION=${RELEASE_TAG}' -X 'main.BUILD_TIME=$(date "+%F %T")' -X 'main.GO_VERSION=$(go version)'-X 'main.GIT_COMMIT_SHA=$(git show -s --format=%H)'"
 # binary suffix
 EXT=''
 ASSETS_EXT='.tar.gz'
@@ -19,42 +20,42 @@ fi
 FILE_SUFFIX=$1-$2${ASSETS_EXT}
 if [ $1 == 'linux' ]&&[ $2 == '386' ]; then
 # linux-386
-CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'linux' ]&&[ $2 == 'amd64' ]; then
 # linux-amd64
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'linux' ]&&[ $2 == 'arm32-v7' ]; then
 # linux-armv7-a
-CC=arm-linux-gnueabihf-gcc-5 CXX=arm-linux-gnueabihf-g++-5 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a -fPIC" CGO_CXXFLAGS="-march=armv7-a -fPIC" go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CC=arm-linux-gnueabihf-gcc-5 CXX=arm-linux-gnueabihf-g++-5 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a -fPIC" CGO_CXXFLAGS="-march=armv7-a -fPIC" go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'linux' ]&&[ $2 == 'arm64' ]; then
 # linux-arm64
-CC=aarch64-linux-gnu-gcc-5 CXX=aarch64-linux-gnu-g++-5 GOOS=linux GOARCH=arm64 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CC=aarch64-linux-gnu-gcc-5 CXX=aarch64-linux-gnu-g++-5 GOOS=linux GOARCH=arm64 CGO_ENABLED=1 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'windows' ]&&[ $2 == 'amd64' ]; then
 # windows-amd64
-CC=x86_64-w64-mingw32-gcc-posix CXX=x86_64-w64-mingw32-g++-posix GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CC=x86_64-w64-mingw32-gcc-posix CXX=x86_64-w64-mingw32-g++-posix GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'windows' ]&&[ $2 == '386' ]; then
 # windows-386
-CC=i686-w64-mingw32-gcc-posix CXX=i686-w64-mingw32-g++-posix GOOS=windows GOARCH=386 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CC=i686-w64-mingw32-gcc-posix CXX=i686-w64-mingw32-g++-posix GOOS=windows GOARCH=386 CGO_ENABLED=1 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'darwin' ]&&[ $2 == 'amd64' ]; then
 # darwin-amd64
-CC=o64-clang CXX=o64-clang++ GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CC=o64-clang CXX=o64-clang++ GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 if [ $1 == 'darwin' ]&&[ $2 == '386' ]; then
 # darwin-386
-CC=o32-clang CXX=o32-clang++ GOOS=darwin GOARCH=386 CGO_ENABLED=1 go build -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
+CC=o32-clang CXX=o32-clang++ GOOS=darwin GOARCH=386 CGO_ENABLED=1 go build -ldflags="$flags" -o ${BUILD_ARTIFACTS_FOLDER}/PanIndex${EXT}
 fi
 
 cp -r LICENSE README.md config/config.json ${BUILD_ARTIFACTS_FOLDER}/
