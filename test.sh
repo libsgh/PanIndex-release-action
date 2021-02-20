bea5b66a3aa7d4a0f2f4f39ca240fe4904dae258
libsgh <woiyyng@gmail.com>
2021-02-20 16:03:12 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eux
+
+# prepare golang
+source /setup-go.sh 
+
+# easy to debug if anything wrong
+go version
+
+git clone https://github.com/libsgh/PanIndex.git
+
+cd PanIndex
+
+flags="-X 'PanIndex/boot.VERSION=v1.0.3' -X 'PanIndex/boot.BUILD_TIME=$(date "+%F %T")' -X 'PanIndex/boot.GO_VERSION=$(go version)'-X 'PanIndex/boot.GIT_COMMIT_SHA=$(git show -s --format=%H)'"
+
+go build -ldflags="$flags" -v -o PanIndex .
+
+./PanIndex
+
+
