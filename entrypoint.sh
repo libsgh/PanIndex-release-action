b2762e0db58432be9ffd2f1c06eb73b19e252e40
libsgh <woiyyng@gmail.com>
2021-02-20 11:31:53 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,17 @@ go version
 
 # build & release go binaries
 /release.sh linux 386
+sleep 30
 /release.sh linux amd64
+sleep 30
 /release.sh linux arm32-v7
+sleep 30
 /release.sh linux arm64
+sleep 30
 /release.sh windows 386
+sleep 30
 /release.sh windows amd64
+sleep 30
 #/release.sh darwin 386
 #/release.sh darwin amd64
 
